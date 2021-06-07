4a14b8cec0d41c7a9a327c37444ed9a05f661581
Connor Torrell <connortorrell@gmail.com>
2021-06-07 10:12:35 -0400

    added test for when asked_player doesn't have card

--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -2,21 +2,19 @@ require_relative "../lib/round"
 require_relative "../lib/card_deck"
 
 describe 'Round' do
-  let(:round) { Round.new(CardDeck.new, Player.new("Connor", [Card.new("ace")]), "ace", Player.new("Matthew", [Card.new("ace")])) }
-
   it "gives ace from asked_player to asking_player" do
+    round = Round.new(CardDeck.new, Player.new("Connor", [Card.new("ace"), Card.new("ace"), Card.new("ace")]), "ace", Player.new("Matthew", [Card.new("ace")]))
     round.play
-    expect(round.asking_player.cards_left).to eq(2)
-    expect(round.asking_player.has_card?("ace")).to eq(true)
+    expect(round.asking_player.cards_left).to eq(4)
+    expect(round.asking_player.books).to eq(1)
     expect(round.asked_player.cards_left).to eq(0)
   end
 
-  it "deals 5 cards to 4 players" do
-    game = Game.new([Player.new("Connor"), Player.new("Jake"), Player.new("Matthew"), Player.new("Brianna")])
-    game.start
-    expect(game.players[0].cards_left).to eq(5)
-    expect(game.players[1].cards_left).to eq(5)
-    expect(game.players[2].cards_left).to eq(5)
-    expect(game.players[3].cards_left).to eq(5)
+  it "gives asking_player a card from the deck when asked_player does not have the rank asked for" do
+    round = Round.new(CardDeck.new, Player.new("Connor", [Card.new("ace"), Card.new("ace"), Card.new("ace")]), "ace", Player.new("Matthew", [Card.new("jack")]))
+    round.play
+    expect(round.asking_player.cards_left).to eq(4)
+    expect(round.asking_player.books).to eq(0)
+    expect(round.asked_player.cards_left).to eq(1)
   end
 end
