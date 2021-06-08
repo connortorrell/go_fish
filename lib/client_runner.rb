5104de472370a32297b4998b4a28b9ddd729b0db
Connor Torrell <connortorrell@gmail.com>
2021-06-08 08:59:29 -0400

    changed client_runner for better use

--- lib/client_runner.rb
+++ lib/client_runner.rb
@@ -6,11 +6,13 @@ print "Enter the port number: "
 port_number = gets.chomp.to_i
 client = Client.new(network, port_number)
 while true do
-  puts "Waiting for other player"
+  puts "Waiting for updates"
   output = ""
   until output != ""
     output = client.capture_output
   end
-  print output
-  client.provide_input(gets.chomp)
+  puts output
+  if output.include?(":")
+    client.provide_input(gets.chomp)
+  end
 end
